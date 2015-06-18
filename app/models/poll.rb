17df4c1a9020ae6f3115b42a996970684907f368
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-06-18 19:26:04 +0700

    Add validations to models

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,7 @@ class Poll < ActiveRecord::Base
   belongs_to  :post
   has_many    :poll_items, dependent: :destroy
   has_many    :votes, through: :poll_items
+
+  validates_presence_of :post_id
+  validates_presence_of :question
 end
--- app/models/poll_item.rb
+++ app/models/poll_item.rb
@@ -2,4 +2,6 @@ class PollItem < ActiveRecord::Base
   belongs_to :poll
   has_many :votes, dependent: :destroy
   attr_readonly :votes_count
+  validates_presence_of :poll_id
+  validates_presence_of :answer
 end
